191242c6069aeeaffa50f074116707328ec52c7c
cassiotakarada <cassiotakarada7@gmail.com>
2024-04-30 15:02:44 -0300

    updating code

--- server/src/app/config/index.ts
+++ server/src/app/config/index.ts
@@ -2,9 +2,15 @@ import { Express } from "express";
 import connectToMongoDB from "../database";
 import appMiddleware from "./middleware";
 
-const appConfig = (app: Express): void => {
-  connectToMongoDB();
-  appMiddleware(app);
+const appConfig = async (app: Express): Promise<void> => {
+  try {
+    await connectToMongoDB();
+    console.log("MongoDB connected successfully.");
+
+    appMiddleware(app);
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+  }
 };
 
 export default appConfig;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,14 @@
 import "dotenv/config";
+console.log("Environment Variables:", {
+  PORT: process.env.PORT,
+  LOCAL_PORT: process.env.LOCAL_PORT,
+  MONGO_URI: process.env.MONGO_URI,
+});
+
 import app from "./app";
 
 const PORT = process.env.PORT || process.env.LOCAL_PORT;
 
 app.listen(PORT, () => {
-  console.log(`server running in ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
