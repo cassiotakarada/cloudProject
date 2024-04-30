08b1649167d0bab18d0ee9c01e682e46af9addd2
cassiotakarada <cassiotakarada7@gmail.com>
2024-04-30 15:05:23 -0300

    updates

--- server/src/app/config/index.ts
+++ server/src/app/config/index.ts
@@ -6,6 +6,11 @@ const appConfig = async (app: Express): Promise<void> => {
   try {
     await connectToMongoDB();
     console.log("MongoDB connected successfully.");
+    console.log("Environment Variables:", {
+      PORT: process.env.PORT,
+      LOCAL_PORT: process.env.LOCAL_PORT,
+      MONGO_URI: process.env.MONGO_URI,
+    });
 
     appMiddleware(app);
   } catch (error) {
